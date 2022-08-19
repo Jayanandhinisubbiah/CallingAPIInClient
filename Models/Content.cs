e8dd75071e94cad29add24a8a3c95ff04a90b69e
Jayanandhini S <jayanandhini@whiteblue.in>
2022-08-19 16:35:45 +0530

    sjdvnsdkjvn

--- Models/Content.cs
+++ Models/Content.cs
@@ -2,10 +2,17 @@
 {
     public class Content
     {
-        public int? Qnt { get; set; }
-        public int? FoodId { get; set; }
+        public string Email { get; set; }
+        //public UserList user { get; set; }
+        public string Image { get; set; }
+        public string FoodName { get; set; }
+        //public Food food { get; set; }
+        public float Price { get; set; }
+        public int Qnt { get; set; }
+        public float TotalPrice { get; set; }
+        //public OrderDetails orderDetails { get; set; }
         
-        public int? UserId { get; set; }
+       
        
     }
 }
