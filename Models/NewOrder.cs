59d10f87a771f21e718fbe87e9d88363f2e88c08
Jayanandhini S <jayanandhini@whiteblue.in>
2022-08-22 11:51:53 +0530

    ksdjcnksdjn

--- Models/NewOrder.cs
+++ Models/NewOrder.cs
@@ -2,6 +2,9 @@
 {
     public class NewOrder
     {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+
         public int count { get; set; }
         public string Email { get; set; }
         //public UserList user { get; set; }
@@ -11,6 +14,8 @@
         public float Price { get; set; }
         public int Qnt { get; set; }
         public float TotalPrice { get; set; }
+        public virtual OrderDetails? OrderDetails { get; set; }
+
         //public OrderDetails orderDetails { get; set; }
 
     }
